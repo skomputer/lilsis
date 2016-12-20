38a0ab5f7f7d60042624b7e73ce0a4d8cbe53b3b
ziggy <ziggy@elephant-bird.net>
2016-12-20 14:17:11 -0500

    Create entity network as a hash

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'pp'
 
 describe Link, type: :model do
 
@@ -7,12 +8,16 @@ describe Link, type: :model do
   it { should belong_to(:related) }
   it { should have_many(:chained_links) }
 
+  
+
   describe 'relationships through links' do
     before(:all) do
+      DatabaseCleaner.start
       @e1 = create(:corp)
       @e2 = create(:corp)
       @rel = Relationship.create!(entity1_id: @e1.id, entity2_id: @e2.id, category_id: Relationship::DONATION_CATEGORY)
     end
+    after(:all) { DatabaseCleaner.clean } 
 
     it 'created links' do
       Link.last(2).each { |link| expect(link.relationship).to eql @rel }
@@ -27,30 +32,88 @@ describe Link, type: :model do
     end
   end
 
-  describe 'entity_network' do
+  describe 'Entity Network' do 
     before(:all) do
-      @e1 = create(:corp)
-      @e2 = create(:corp)
-      @e3 = create(:corp)
-      @e4 = create(:corp)
+      DatabaseCleaner.start
+      @e1 = create(:corp, name: 'e1')
+      @e2 = create(:corp, name: 'e2')
+      @e3 = create(:corp, name: 'e3')
+      @e4 = create(:corp, name: 'e4')
       @rel_between_1_2 = Relationship.create!(entity1_id: @e1.id, entity2_id: @e2.id, category_id: Relationship::DONATION_CATEGORY)
       @rel_between_2_3 = Relationship.create!(entity1_id: @e2.id, entity2_id: @e3.id, category_id: Relationship::DONATION_CATEGORY)
       @rel_between_3_4 = Relationship.create!(entity1_id: @e3.id, entity2_id: @e4.id, category_id: Relationship::DONATION_CATEGORY)
-      
     end
     
-    it 'returns list of entity ids one and two degrees away from provided entity' do
-      expect(Set.new(Link.entity_network(@e1.id))).to eql Set.new([@e1.id, @e2.id, @e3.id])
+    after(:all) { DatabaseCleaner.clean } 
+    
+    describe 'entity_network_simple' do
+      it 'returns list of entity ids one and two degrees away from provided entity' do
+        expect(Set.new(Link.entity_network_simple(@e1.id))).to eql Set.new([@e1.id, @e2.id, @e3.id])
+      end
+      it 'returns list if provided <entity> ' do
+        expect(Set.new(Link.entity_network_simple(@e1))).to eql Set.new([@e1.id, @e2.id, @e3.id])
+      end
+      it 'returns list of entity models' do
+        expect(Set.new(Link.entity_network_simple(@e1, true))).to eql Set.new([@e1, @e2, @e3])
+      end
     end
 
-    it 'returns list if provided <entity> ' do
-      expect(Set.new(Link.entity_network(@e1))).to eql Set.new([@e1.id, @e2.id, @e3.id])
-    end
+    describe 'entity_network' do    
+      def e2_hash
+        { @e2.id => {
+          entity: { id: @e2.id,blurb: @e2.blurb, name: @e2.name, primary_ext: @e2.primary_ext },
+          links: [ {degree: 1, relationship_id: @rel_between_1_2.id, related_through: nil  }]
+        }
+        }
+      end
 
-    it 'returns list of entity models' do
-      expect(Set.new(Link.entity_network(@e1, true))).to eql Set.new([@e1, @e2, @e3])
-    end
+      def e3_hash
+        { @e3.id => {
+          entity: { id: @e3.id,blurb: @e3.blurb, name: @e3.name, primary_ext: @e3.primary_ext },
+          links: [ {degree: 2, relationship_id: @rel_between_2_3.id, related_through: @e2.id  }]
+        }
+        }
+      end
+      
+      it 'returns an hash with :links and :entity' do
+        expect(Link.entity_network(@e1)).to be_a Hash
+        
+      end
 
+      it 'has length of 2' do
+        expect(Link.entity_network(@e1).length).to eql 2
+      end
+
+      it 'has entity hash and link array' do
+        Link.entity_network(@e1).each do |key, val|
+          expect(val.fetch :entity).to be_a Hash
+          expect(val.fetch :links).to be_a Array
+        end
+      end
+      
+      it 'entity hashes have correct structure' do
+        Link.entity_network(@e1).each do |key, val|
+          expect(val[:entity]).to have_key :id
+          expect(val[:entity]).to have_key :blurb
+          expect(val[:entity]).to have_key :name
+          expect(["Org", "Person"]).to include(val[:entity][:primary_ext])
+        end
+      end
+
+      it 'link hashes have correct structure' do
+        Link.entity_network(@e1).each do |key, val|
+          val.fetch(:links).each do |link|
+            expect([1,2]).to include(link[:degree])
+            expect(link).to have_key :relationship_id
+            expect(link).to have_key :related_through
+          end
+        end
+      end
+      
+      it 'reutrns correct data' do
+        expect(Link.entity_network(@e1)).to eql e2_hash.merge(e3_hash)
+      end
+
+    end
   end
-
 end
