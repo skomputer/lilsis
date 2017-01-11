2f4b82db4ca0cab7ab034021ccc414644626815f
ziggy <ziggy@littlesis.org>
2017-01-11 16:21:01 -0500

    add mnuchin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,13 +8,14 @@ class PagesController < ApplicationController
   def regime
     @pauson = Entity.find(15032)
     @schwarzman = Entity.find(14997)
-    render layout: "fullscreen"
+    @mnuchin = Entity.find(1761)
+    render layout: 'fullscreen'
   end
 
   def the_trump_club
     @trump = Entity.find(15108)
     @graph = trump_network_graph
-    render layout: "fullscreen"
+    render layout: 'fullscreen'
   end
 
   def trump_network
